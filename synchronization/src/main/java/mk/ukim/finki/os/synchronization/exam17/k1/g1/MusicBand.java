1ebd8443198290d470f3dac32ea326ee0331c7d0
drilonsaiti <50522333+drilonsaiti@users.noreply.github.com>
2020-04-14 17:27:48 +0200

    Update MusicBand.java

--- synchronization/src/main/java/mk/ukim/finki/os/synchronization/exam17/k1/g1/MusicBand.java
+++ synchronization/src/main/java/mk/ukim/finki/os/synchronization/exam17/k1/g1/MusicBand.java
@@ -8,42 +8,84 @@ import java.util.HashSet;
 import java.util.Scanner;
 
 public class MusicBand {
+  
+    static Semaphore singer;
+    static Semaphore guitar;
 
+    static Semaphore singerHere;
 
-  static MusicBandState state = new MusicBandState();
+    static Semaphore ready;
+    static Semaphore finished;
 
-  public static class GuitarPlayer extends TemplateThread {
+    static Semaphore lock;
+    static int countGuitar;
+    public static void init() {
+        singer = new Semaphore(2);
+        guitar = new Semaphore(3);
 
-    public GuitarPlayer(int numRuns) {
-      super(numRuns);
+        singerHere = new Semaphore(0);
+
+        ready = new Semaphore(0);
+        finished = new Semaphore(0);
+
+        lock = new Semaphore(1);
+        countGuitar = 0;
     }
 
-    @Override
-    public void execute() throws InterruptedException {
-      state.play();
-      state.evaluate();
+    public static class GuitarPlayer extends TemplateThread {
+
+        public GuitarPlayer(int numRuns) {
+            super(numRuns);
+        }
+
+        @Override
+        public void execute() throws InterruptedException {
+            guitar.acquire();
+
+            lock.acquire();
+            countGuitar++;
+            if(countGuitar == 3){
+                singerHere.acquire(2);
+                ready.release(5);
+            }
+            lock.release();
+
+            ready.acquire();
+            state.play();
+            finished.release();
+
+            lock.acquire();
+            countGuitar--;
+            if(countGuitar == 0){
+                finished.acquire(5);
+                state.evaluate();
+                guitar.release(3);
+                singer.release(2);
+            }
+            lock.release();
+        }
+
     }
 
-  }
+    public static class Singer extends TemplateThread {
 
-  public static class Singer extends TemplateThread {
+        public Singer(int numRuns) {
+            super(numRuns);
+        }
 
-    public Singer(int numRuns) {
-      super(numRuns);
+        @Override
+        public void execute() throws InterruptedException {
+            singer.acquire();
+            singerHere.release();
+            ready.acquire();
+            state.play();
+            finished.release();
+        }
+
     }
-
-    @Override
-    public void execute() throws InterruptedException {
-      state.play();
-
-    }
-
-  }
-
-  public static void init() {
-
-  }
-
+  
+ static MusicBandState state = new MusicBandState();
+  
   public static void main(String[] args) {
     for (int i = 0; i < 10; i++) {
       run();
